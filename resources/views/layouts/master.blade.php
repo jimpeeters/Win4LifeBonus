4b59baedce11965cd1136fdcf10f258685f4b38e
Jim Peeters <Jim Peeters>
2015-10-08 14:17:59 +0200

    Master blade + homepage begin

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Win For Life Bonus - @yield('title')</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="stylesheet/style.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
